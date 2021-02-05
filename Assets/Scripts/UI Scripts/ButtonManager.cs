cbb093d60c532e644d51eef83d32c928ccdbeea8
Gabtwotwo <Gabtwotwo@github.com>
2021-02-05 18:58:33 -0500

    Added music and sfx

--- Assets/Scripts/UI Scripts/ButtonManager.cs
+++ Assets/Scripts/UI Scripts/ButtonManager.cs
@@ -5,22 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
+    public AudioClip buttonPress;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void PlayButton()
     {
+        audioSource.PlayOneShot(buttonPress, 0.8f);
         SceneManager.LoadScene("Game");
     }
     public void GameOverButton()
     {
+        audioSource.PlayOneShot(buttonPress, 0.8f);
+
         SceneManager.LoadScene("GameOver");
     }
 
     public void ReturnToMenuButton()
     {
+        audioSource.PlayOneShot(buttonPress, 0.8f);
+
         SceneManager.LoadScene("MainMenu");
     }
 
     public void QuitButton()
     {
+        audioSource.PlayOneShot(buttonPress, 0.8f);
+
         Application.Quit();
     }
 }
